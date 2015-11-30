54a4827cce2c6bfd93f01b9dd7017600bf282978
fuquanemail@gmail.com <fuquanemail@gmail.com>
2015-11-30 23:20:56 +0800

    update

---
+++ spring-webmvc/src/main/java/webmvc/interceptor/HandlerInterceptor2.java
@@ -0,0 +1,23 @@
+package webmvc.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * spring-demo 2015/6/22 11:43
+ * fuquanemail@gmail.com
+ */
+public class HandlerInterceptor2 extends HandlerInterceptorAdapter {
+    static Logger logger = LoggerFactory.getLogger(HandlerInterceptor2.class);
+
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("HandlerInterceptor2 ...  [ preHandle ] ", handler.toString());
+
+    }
+
+}
