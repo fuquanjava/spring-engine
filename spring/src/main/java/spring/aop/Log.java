2ee744c59d0008096544208f6e13657957dadbc6
付泉 <3040408640@qq.com>
2016-02-18 11:52:36 +0800

    1.  update

---
+++ spring/src/main/java/spring/aop/Log.java
@@ -0,0 +1,17 @@
+package spring.aop;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * spring-engine 2015/10/24 22:42
+ * fuquanemail@gmail.com
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Log {
+    String value();
+}
