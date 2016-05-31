26003d1adf3ce59fff5d0dd93b8bb32da2f96a10
付泉 <3040408640@qq.com>
2016-05-31 08:55:59 +0800

    1. rpc

--- spring/src/test/java/spring/rpc/test/ClientStart.java
+++ spring/src/test/java/spring/rpc/test/ClientStart.java
@@ -13,6 +13,7 @@ import spring.rpc.service.HelloService;
  */
 public class ClientStart {
     public static void main(String[] args) {
+        //http://my.oschina.net/huangyong/blog/361751?fromerr=50U1CUbH
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-rpc-client.xml");
         RpcProxy rpcProxy = context.getBean(RpcProxy.class);
         HelloService helloService = rpcProxy.create(HelloService.class);
