0180deea5818c3df071a86b9ef2d67d1fad1f6c6
fuquanemail@gmail.com <fuquanemail@gmail.com>
2016-07-31 23:13:18 +0800

    1. redis

--- spring-nosql/src/test/java/nosql/redis/jedis/JedisClusterTest.java
+++ spring-nosql/src/test/java/nosql/redis/jedis/JedisClusterTest.java
@@ -1,49 +1,107 @@
 package nosql.redis.jedis;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.util.JedisClusterCRC16;
 
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * fuquanemail@gmail.com
- */
 public class JedisClusterTest {
-    private static final int DEFAULT_TIMEOUT = 2000;
-    private static final int DEFAULT_REDIRECTIONS = 3;
-    private static final JedisPoolConfig DEFAULT_CONFIG = new JedisPoolConfig();
+
+    static Logger log = LoggerFactory.getLogger(JedisClusterTest.class);
+
+    JedisCluster cluster = null;
+
+    @Before
+    public void jedisCluster() {
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("192.168.1.128", 7000));
+        nodes.add(new HostAndPort("192.168.1.128", 7001));
+        nodes.add(new HostAndPort("192.168.1.128", 7002));
+        nodes.add(new HostAndPort("192.168.1.128", 7003));
+        nodes.add(new HostAndPort("192.168.1.128", 7004));
+        nodes.add(new HostAndPort("192.168.1.128", 7005));
 
 
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMaxIdle(2);
+        poolConfig.setMaxTotal(5);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setMaxWaitMillis(6 * 1000);
+
+        // public JedisCluster(Set<HostAndPort> set, final GenericObjectPoolConfig poolConfig) {}
+        cluster = new JedisCluster(nodes, poolConfig);
+
+        System.out.println(cluster);
+
+        Map<String, JedisPool> poolMap = cluster.getClusterNodes();
+
+        Iterator<String> iterator = poolMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            JedisPool pool = poolMap.get(key);
+            System.out.println("key:" + key + ", JedisPool:" + pool);
+
+        }
+    }
+
+    @Test
+    public void set() {
+        int slot = JedisClusterCRC16.getSlot("a");
+        log.info("slot:{}", slot);
+
+        String ok = cluster.set("a", "123");
+        log.info("response is ok ? {}", ok);
+
+        String value = cluster.get("a");
+        log.info("value :{}", value);
+
+
+    }
 
 
     @Test
-    public void testDiscoverNodesAutomatically() throws Exception {
+    public void test() {
+        int num = 1000;
+        String key = "key.";
+        String value = "value.";
+        while (true) {
+            for (int i = 1; i <= num; i++) {
+                try {
+                    // 存数据
+                    String ok = cluster.set(key + i, value + i);
+                    log.info("set {} is ok ? : {}", key + i, ok);
 
-        Set<HostAndPort> jedisClusterNode = new HashSet<>();
-        jedisClusterNode.add(new HostAndPort("192.168.1.128", 7000));
+                    // 取数据
+                    String rsp = cluster.get(key + i);
 
-        JedisCluster cluster = new JedisCluster(jedisClusterNode, DEFAULT_TIMEOUT);
+                    log.info("get {} value : {}", key + i, rsp);
 
-        System.out.println(cluster.getClusterNodes().size());
-
-        Map<String, JedisPool> jedisPoolMap = cluster.getClusterNodes();
-
-        Set<String> keySet = jedisPoolMap.keySet();
-
-        Iterator<String> iterable = keySet.iterator();
-
-        while (iterable.hasNext()){
-            String key = iterable.next();
-            JedisPool pool = jedisPoolMap.get(key);
-
-            System.out.println("key="+ key +", pool="+ pool);
+                    if (StringUtils.isEmpty(value)) {
+                        log.error("===>break" + key + i + " value is null");
+                        break;
+                    }
+                } catch (Exception e) {
+                    log.error("====>", e);
+                    continue;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
+
 }
